bc9a63c876ab419d4353d16f59749bed225cd5d5
aperigno <alexisperignon@live.fr>
2019-04-19 12:00:03 +0200

    environnement

--- src/ubordeaux/deptinfo/compilation/project/environment/Environment.java
+++ src/ubordeaux/deptinfo/compilation/project/environment/Environment.java
@@ -1,24 +1,26 @@
 package ubordeaux.deptinfo.compilation.project.environment;
 import java.util.HashMap;
+
+import ubordeaux.deptinfo.compilation.project.type.Type;
 import java.util.Map;
 
 public class Environment implements EnvironmentInt {
 
-	private Map<String, Double> table;
+	private Map<String, Type> table;
 	private String str;
 	public Environment(String str) {
-		this.table = new HashMap<String, Double>();
+		this.table = new HashMap<String, Type>();
 		this.str = str;
 	}
 
 	@Override
-	public void putVariable(String variable, Double value) {
+	public void putVariable(String variable, Type value) {
 		table.put(variable, value);
 		System.out.println("Enregistre " + variable + " " + value);
 	}
 
 	@Override
-	public Double getVariableValue(String variable) {
+	public Type getVariableValue(String variable) {
 		System.out.println("Retrouve " + variable + " " + table.get(variable));
 		return table.get(variable);
 	}
--- src/ubordeaux/deptinfo/compilation/project/environment/EnvironmentInt.java
+++ src/ubordeaux/deptinfo/compilation/project/environment/EnvironmentInt.java
@@ -1,9 +1,9 @@
 package ubordeaux.deptinfo.compilation.project.environment;
-
+import ubordeaux.deptinfo.compilation.project.type.Type;
 public interface EnvironmentInt {
 	
-	void putVariable(String var, Double value);
-	Double getVariableValue(String variable);
+	void putVariable(String var, Type value);
+	Type getVariableValue(String variable);
 	
 
 }
