ae7ad07ad3c953bb12f14cfb0f54977d24b68ac2
aperigno <alexisperignon@live.fr>
2019-04-20 14:54:02 +0200

    les fonctions c'est bon

--- src/ubordeaux/deptinfo/compilation/project/environment/Environment.java
+++ src/ubordeaux/deptinfo/compilation/project/environment/Environment.java
@@ -2,15 +2,20 @@ package ubordeaux.deptinfo.compilation.project.environment;
 import java.util.HashMap;
 
 import ubordeaux.deptinfo.compilation.project.type.Type;
+import ubordeaux.deptinfo.compilation.project.type.TypeFeature;
+import ubordeaux.deptinfo.compilation.project.type.TypeList;
+
 import java.util.Map;
 
 public class Environment implements EnvironmentInt {
 
 	private Map<String, Type> table;
 	private String str;
+	
 	public Environment(String str) {
 		this.table = new HashMap<String, Type>();
 		this.str = str;
+		
 	}
 
 	@Override
@@ -24,6 +29,20 @@ public class Environment implements EnvironmentInt {
 		System.out.println("Retrouve " + variable + " " + table.get(variable));
 		return table.get(variable);
 	}
+	public void putFunction(String variable, Type value) {
+		table.put(variable, value);
+		//table.put(variable, );
+		System.out.println("Enregistre " + variable + " " + value);
+	}
+
+	
+	public Type getFunction(String variable) {
+		System.out.println("Retrouve " + variable + " " + table.get(variable));
+		return table.get(variable);
+	}
+
+	
+	
 
 
 }
