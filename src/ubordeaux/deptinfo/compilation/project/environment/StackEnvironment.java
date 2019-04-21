19af6f4fa8a2fe68daad7090738b8d52aaceb412
aperigno <alexisperignon@live.fr>
2019-04-21 18:53:26 +0200

    Stack variables locales done

--- src/ubordeaux/deptinfo/compilation/project/environment/StackEnvironment.java
+++ src/ubordeaux/deptinfo/compilation/project/environment/StackEnvironment.java
@@ -1,8 +1,56 @@
 package ubordeaux.deptinfo.compilation.project.environment;
 
+
+import ubordeaux.deptinfo.compilation.project.type.Type;
+import ubordeaux.deptinfo.compilation.project.type.TypeVoid;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+
+
+
 public class StackEnvironment {
+
 	private String str;
+
+	private Stack<Map<String, Type>> stack;
+	
 	public StackEnvironment(String str) {
+		//this.table = new HashMap<String, Type>();
+		this.stack = new Stack<Map<String, Type>>();
 		this.str = str;
+	}
+	
+	public void putVariable(String variable, Type value) {
+		if(!this.stack.empty()) 
+			stack.peek().put(variable, value);
+		System.out.println("Enregistre dans stack   " + variable + " " + value );
+	}
+	
+
+
+	public Type getVariableValue(String variable) {
+		if(!this.stack.isEmpty()) {
+		stack.peek().get(variable);
+		System.out.println("Retrouve dans stack   " + variable + " " + stack.peek().get(variable));
+		return stack.peek().get(variable);
+		}
+		return new TypeVoid();
+		
+	}
+	
+	public void push_stack() {
+		System.out.println("Stack PUSH");
+		this.stack.push(new HashMap<String, Type>());
+	}
+	public void pop_stack() {
+		if(!this.stack.isEmpty()) {
+			
+			this.stack.pop();
+			System.out.println("Stack PUSH");
+		}
+		
 	}
 }
