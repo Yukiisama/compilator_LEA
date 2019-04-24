9fa4177b4a3f8edb57f8abf7ac2c4abbbd59b923
Jean-David <akata.jda@gmail.com>
2019-04-24 16:31:46 +0200

    CI version mercredi

--- src/ubordeaux/deptinfo/compilation/project/intermediateCode/Binop.java
+++ src/ubordeaux/deptinfo/compilation/project/intermediateCode/Binop.java
@@ -14,9 +14,42 @@ public class Binop extends Exp {
 	public final static int PLUS = 0, MINUS = 1, MUL = 2, DIV = 3, AND = 4, OR = 5, LSHIFT = 6, RSHIFT = 7, ARSHIFT = 8,
 			XOR = 9;
 
-	public String toString() {
-		String result = new String();
-		return result;
-	}
+	 public String toString() {
+	        String BOP="";
+	        switch(binop) {
+	        case 0 :
+	            BOP ="PLUS";
+	            break;
+	        case 1:
+	            BOP="MINUS";
+	            break;
+	        case 2:
+	            BOP="MUL";
+	            break;
+	        case 3:
+	            BOP="DIV";
+	            break;
+	        case 4:
+	            BOP="AND";
+	            break;
+	        case 5:
+	            BOP="OR";
+	            break;
+	        case 6:
+	            BOP="LSHIFT";
+	            break;
+	        case 7:
+	            BOP="RSHIFT";
+	            break;
+	        case 8:
+	            BOP="ARSHIFT";
+	            break;
+	        case 9:
+	            BOP="XOR";
+	            break;
+	        default: break;
+	        }
+	        return "IC: Binop " + left.toString() + BOP + right.toString();
+	    }
 
 }
